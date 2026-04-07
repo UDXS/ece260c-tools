e3730069b3be465b8fb1b31fe03bbe5cd24cd878
Davit Markarian <dmarkarian@ucsd.edu>
2026-04-06 23:31:47 -0700

    gui_start executable

--- unified/prep_gui.sh
+++ unified/prep_gui.sh
@@ -1,7 +1,8 @@
 apt update
-apt install -y dbus-x11 shotwell geany
+apt install -y dbus-x11 shotwell geany tigervnc-standalone-server htop btop xfce4 xfce4-terminal
 
 cp /usr/local/share/applications/openroad.desktop ~/Desktop
 cp /usr/share/applications/klayout.desktop ~/Desktop
 
 echo "vncserver -localhost -SecurityTypes None -geometry 1920x1200 :5 -- xfce4-session && export DISPLAY=:5" > /usr/bin/gui_start
+chmod +x /usr/bin/gui_start
