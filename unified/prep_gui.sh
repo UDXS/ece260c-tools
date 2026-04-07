25c1278666095ccebbd82f22da4e829ae24c06bd
Davit Markarian <dmarkarian@ucsd.edu>
2026-04-07 00:14:48 -0700

    Fix desktop shortcut chmod

--- unified/prep_gui.sh
+++ unified/prep_gui.sh
@@ -3,7 +3,9 @@ apt install -y dbus-x11 shotwell geany tigervnc-standalone-server htop btop xfce
 
 mkdir /root/Desktop
 cp /usr/local/share/applications/openroad.desktop ~/Desktop
+chmod +x ~/openroad.desktop
 cp /usr/share/applications/klayout.desktop ~/Desktop
+chmod +x ~/klayout.desktop
 
 echo "vncserver -localhost -SecurityTypes None -geometry 1920x1200 :5 -- xfce4-session && export DISPLAY=:5" > /usr/bin/gui_start
 chmod +x /usr/bin/gui_start
